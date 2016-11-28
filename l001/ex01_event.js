5d15b4aecb1f3d120b7f2d03ede453d54864f7d0
PavelZ <grosesdorf@outlook.com>
2016-11-28 21:56:46 +0200

    edit

---
+++ l001/ex01_event.js
@@ -0,0 +1,24 @@
+// События только на конкретный элемент
+// document.querySelector("#big").addEventListener("click", function(){
+// 	console.log("Big");
+// });
+// document.querySelector("#small").addEventListener("click", function(){
+// 	console.log("Small");
+// 	event.stopPropagation();
+// });
+
+// Событие снизу вверх
+// document.querySelector("#big").addEventListener("click", function(){
+// 	console.log("Big");
+// }, true);
+// document.querySelector("#small").addEventListener("click", function(){
+// 	console.log("Small");
+// });
+
+// Событие сверху вниз
+// document.querySelector("#big").addEventListener("click", function(){
+// 	console.log("Big");
+// });
+// document.querySelector("#small").addEventListener("click", function(){
+// 	console.log("Small");
+// });
